4f1cdd2f5a66d3dc0eea89eb3ed309ac8aad01a0
Aquinter10 <124784123+Aquinter10@users.noreply.github.com>
2025-05-25 14:46:14 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Proyecto Análisis Numérico</h1>
+      <div className="button-group">
+        <a href="https://aquinter10.github.io/equation-methods" className="button">Capitulo 1</a>
+        <a href="https://usuario.github.io/proyecto2" className="button">Proyecto 2</a>
+        <a href="https://usuario.github.io/proyecto3" className="button">Proyecto 3</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
