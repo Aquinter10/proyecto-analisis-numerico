64eaa2946a53d21b967ee8115e80f48a0de5ca6e
santiagosteam5 <facsonnmartens@gmail.com>
2025-05-28 15:03:45 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ function App() {
         <a href="https://usuario.github.io/proyecto2" className="button">Proyecto 2</a>
         <a href="https://aquinter10.github.io/interpolation-methods/" className="button">Capitulo 3</a>
       </div>
+      <div class="tenor-gif-embed" data-postid="2761164463363223324" data-share-method="host" data-aspect-ratio="1.31746" data-width="100%"><a href="https://tenor.com/view/triple-baka-teto-miku-neru-gif-2761164463363223324">Triple Baka Teto GIF</a>from <a href="https://tenor.com/search/triple+baka-gifs">Triple Baka GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
     </div>
+    
   );
 }
 
