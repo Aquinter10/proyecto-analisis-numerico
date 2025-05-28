8b295c6f9aed08525184c9e5d6fc35487f30e2d7
santiagosteam5 <facsonnmartens@gmail.com>
2025-05-28 18:59:44 -0500

    updated with all chapter links

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ function App() {
       <h1>Proyecto Análisis Numérico</h1>
       <div className="button-group">
         <a href="https://aquinter10.github.io/equation-methods" className="button">Capitulo 1</a>
-        <a href="https://usuario.github.io/proyecto2" className="button">Proyecto 2</a>
+        <a href="https://santiagosteam5.github.io/system-equations-methods/" className="button">Capitulo 2</a>
         <a href="https://aquinter10.github.io/interpolation-methods/" className="button">Capitulo 3</a>
       </div>
-      <div class="tenor-gif-embed" data-postid="2761164463363223324" data-share-method="host" data-aspect-ratio="1.31746" data-width="100%"><a href="https://tenor.com/view/triple-baka-teto-miku-neru-gif-2761164463363223324">Triple Baka Teto GIF</a>from <a href="https://tenor.com/search/triple+baka-gifs">Triple Baka GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
     </div>
-    
   );
 }
 
